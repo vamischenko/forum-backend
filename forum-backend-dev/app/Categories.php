22a0b41038c883a2d962be6401ec8aabb8958d35
Dmitry <dmitrybosykh@Dmitrys-MacBook-Pro-2.local>
2018-11-29 21:59:47 +0700

    add git

--- forum-backend-dev/app/Categories.php
+++ forum-backend-dev/app/Categories.php
@@ -7,12 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Categories extends Model
 {
     protected $fillable = ['name', 'status', 'avatar', 'description', 'created_at', 'updated_at'];
-
-    # Фунция связи с таблицей Topics
-
-//    public function getTopics() {
-//
-//       return $this->hasMany(Topics::class)->get();
-//    }
 }
 
