6d1b9c4f5171afd8d29e36c125942908dde81632
turngait <wulzin.t@gmail.com>
2018-11-30 12:46:38 +0400

    Роутинг. добавил хедары

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 header( 'Access-Control-Allow-Headers', 'X-Requested-With, Authorization, Content-Type' );
 
